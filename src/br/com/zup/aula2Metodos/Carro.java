6d71d47b1dbd7d3891be4fca1023a67d0e3f2ba4
Raline Silva <ralinesilva1@gmail.com>
2021-09-20 14:30:41 -0300

    primeiro commit

---
+++ src/br/com/zup/aula2Metodos/Carro.java
@@ -0,0 +1,28 @@
+package br.com.zup.aula2Metodos;
+/*Crie uma classe que represente um carro, que tenha como características o
+    nome, o peso, a fabricante, a cor e o valor do carro. Crie também o método
+    darDesconto() que servirá para diminuir o preço do carro e o método pintar(), que
+    será responsável por mudar a cor do carro. Crie um carro no seu programa
+    principal, utilize os métodos criados e mostre na tela o preço e a cor do carro antes
+    e depois da utilização dos métodos.
+     */
+public class Carro {
+
+    String nome;
+    double peso;
+    String fabricante;
+    String cor;
+    double valor = 10000;
+
+    public Carro(String nome, double peso, String fabricante, String cor, double valor) {
+        this.nome = nome;
+        this.peso = peso;
+        this.fabricante = fabricante;
+        this.cor = cor;
+        this.valor = valor;
+    }
+
+    public void desconto (double darDesconto){
+        valor = valor - darDesconto;
+    }
+}
---
+++ src/br/com/zup/aula2Metodos/Main.java
@@ -0,0 +1,8 @@
+package br.com.zup.aula2Metodos;
+
+public class Main {
+    public static void main(String[] args) {
+        Carro veiculo = new Carro("Celta",890,"Chevrolet", "Preto",10000);
+        veiculo.desconto(0.1);
+    }
+}
