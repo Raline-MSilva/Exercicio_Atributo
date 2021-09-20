dbc8f21d2b561f03ce40f2df3c0d56854016d758
Raline Silva <ralinesilva1@gmail.com>
2021-09-20 15:31:47 -0300

    saque

--- src/br/com/zup/aula2Metodos/Main.java
+++ src/br/com/zup/aula2Metodos/Main.java
@@ -1,6 +1,18 @@
 package br.com.zup.aula2Metodos;
 
 public class Main {
-    ContaBancaria cb = new ContaBancaria("12345",500, "Benedito");
+    public static void main(String[] args) {
+        ContaBancaria cb = new ContaBancaria("12345",500, "Benedito");
+
+        //mostrando dados da conta
+        cb.conta();
+        System.out.println("-------------");
+
+        //realizando um saque
+        cb.saque(50);
+        cb.conta();
+        System.out.println("---------");
+
+    }
 
 }
