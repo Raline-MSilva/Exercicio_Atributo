86c74a0621ef79b38a51bbe30324367eedc2c924
Raline Silva <ralinesilva1@gmail.com>
2021-09-21 11:54:00 -0300

    iniciando ex2 quadrado

---
+++ src/br/com/zup/aula2Metodos/Complementar/Quadrado.java
@@ -0,0 +1,17 @@
+package br.com.zup.aula2Metodos.Complementar;
+/*
+Classe Quadrado: Crie uma classe que modele um quadrado:
+1. Atributos: Tamanho do lado
+2. Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular
+Área;
+ */
+public class Quadrado {
+    double tamanho;
+    double lado;
+
+    public Quadrado(double tamanho, double lado){
+        this.tamanho = tamanho;
+        this.lado = lado;
+    }
+
+}
