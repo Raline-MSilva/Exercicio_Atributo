30a716875c50900d444daedb019ec8cc603ec5ee
Raline Silva <ralinesilva1@gmail.com>
2021-09-21 11:32:01 -0300

    definindo atributos e método construtor

---
+++ src/br/com/zup/aula2Metodos/Complementar/Bola.java
@@ -0,0 +1,13 @@
+package br.com.zup.aula2Metodos.Complementar;
+
+public class Bola {
+    String cor;
+    String circuferencia;
+    String material;
+
+    public Bola(String cor, String circuferencia, String material){
+        this.cor = cor;
+        this.circuferencia = circuferencia;
+        this.material = material;
+    }
+}
