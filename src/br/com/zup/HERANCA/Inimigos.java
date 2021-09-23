7e21fdd9b4a0eccd134c89336fcb4eb9495f3a47
Raline Silva <ralinesilva1@gmail.com>
2021-09-23 15:17:13 -0300

    incompleto

---
+++ src/br/com/zup/HERANCA/Inimigos.java
@@ -0,0 +1,36 @@
+package br.com.zup.HERANCA;
+
+public class Inimigos {
+        private String nome;
+        private double vida;
+
+        public Inimigos (String nome, double vida) {
+            this.nome = nome;
+            this.vida = vida;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public double getVida() {
+            return vida;
+        }
+
+        public void setVida(double vida) {
+            this.vida = vida;
+        }
+
+        public void trocarNome() {
+            String nome = this.getNome();
+            this.setNome(nome);
+        }
+
+        public double exibirVida(){
+            return exibirVida();
+        }
+}
