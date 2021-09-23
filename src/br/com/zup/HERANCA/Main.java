daae4d7d8abf60b7344a0b8c406ed5707ae65c46
Raline Silva <ralinesilva1@gmail.com>
2021-09-23 15:19:13 -0300

    classe main

---
+++ src/br/com/zup/HERANCA/Main.java
@@ -0,0 +1,12 @@
+package br.com.zup.HERANCA;
+
+public class Main {
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            Magos mago = new Magos("Picles", 50, 80, 90);
+            System.out.println(mago.getVida());
+            System.out.println(mago.getAplicarMagia());
+
+        }
+    }
+}
