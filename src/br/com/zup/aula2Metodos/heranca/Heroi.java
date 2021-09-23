15521943bf16f75ac9344ec2548eeaf79daaaeff
Raline Silva <ralinesilva1@gmail.com>
2021-09-23 17:02:12 -0300

    classe heroi

---
+++ src/br/com/zup/aula2Metodos/heranca/Heroi.java
@@ -0,0 +1,39 @@
+package br.com.zup.aula2Metodos.heranca;
+
+public class Heroi {
+    private String nome;
+    private double vida;
+
+    public Heroi() {
+    }
+
+    public Heroi(String nome, double vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
+    public void trocar(String nome){
+        this.setNome(nome);
+    }
+    public void receber(double dano){
+        vida = vida - dano;
+    }
+    public void exibirVida(){
+        System.out.println("Vida: " + vida);
+    }
+}
