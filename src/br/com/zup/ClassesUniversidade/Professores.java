dc96edef612e7c74c3210ec87163e743dcdc1552
Raline Silva <ralinesilva1@gmail.com>
2021-09-24 15:35:47 -0300

    Subclasse Professores

---
+++ src/br/com/zup/ClassesUniversidade/Professores.java
@@ -0,0 +1,51 @@
+package br.com.zup.ClassesUniversidade;
+
+public class Professores extends Funcionarios{
+    private String nivelGraduacao;
+    private String disciplinaMinistrada;
+    private int qntAlunos;
+    private int qntTurmas;
+
+    public Professores() {
+    }
+
+    public Professores(String nome, String cpf, String numeroRegistro, String orgaoLotacao, double salario, String nivelGraduacao, String disciplinaMinistrada, int qntAlunos, int qntTurmas) {
+        super(nome, cpf, numeroRegistro, orgaoLotacao, salario);
+        this.nivelGraduacao = nivelGraduacao;
+        this.disciplinaMinistrada = disciplinaMinistrada;
+        this.qntAlunos = qntAlunos;
+        this.qntTurmas = qntTurmas;
+    }
+
+    public String getNivelGraduacao() {
+        return nivelGraduacao;
+    }
+
+    public void setNivelGraduacao(String nivelGraduacao) {
+        this.nivelGraduacao = nivelGraduacao;
+    }
+
+    public String getDisciplinaMinistrada() {
+        return disciplinaMinistrada;
+    }
+
+    public void setDisciplinaMinistrada(String disciplinaMinistrada) {
+        this.disciplinaMinistrada = disciplinaMinistrada;
+    }
+
+    public int getQntAlunos() {
+        return qntAlunos;
+    }
+
+    public void setQntAlunos(int qntAlunos) {
+        this.qntAlunos = qntAlunos;
+    }
+
+    public int getQntTurmas() {
+        return qntTurmas;
+    }
+
+    public void setQntTurmas(int qntTurmas) {
+        this.qntTurmas = qntTurmas;
+    }
+}
