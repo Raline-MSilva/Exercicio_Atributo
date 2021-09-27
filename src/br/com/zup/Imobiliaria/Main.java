82c11d194ea87e2a38c85d829fd0ea55338289ce
Raline Silva <ralinesilva1@gmail.com>
2021-09-27 16:45:57 -0300

    Add moradores e imóvel

--- src/br/com/zup/Imobiliaria/Main.java
+++ src/br/com/zup/Imobiliaria/Main.java
@@ -2,6 +2,17 @@ package br.com.zup.Imobiliaria;
 
 public class Main {
     public static void main(String[] args) {
+        Imovel imovel1 = new Imovel("Bahia",800,"Pedro");
+        Moradores morador1 = new Moradores("Raline", "125478");
+        Moradores morador2 = new Moradores("Maria", "125478");
+
+        //add moradores
+        imovel1.addMorador(morador1);
+        imovel1.addMorador(morador2);
+
+        //add imovel
+        System.out.println(imovel1);
+
 
     }
 }
