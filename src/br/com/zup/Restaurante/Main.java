3054fec1123c400bdec7079afe230bda0d18dac8
Raline Silva <ralinesilva1@gmail.com>
2021-09-29 08:34:21 -0300

    Cadastrar Prato

--- src/br/com/zup/Restaurante/Main.java
+++ src/br/com/zup/Restaurante/Main.java
@@ -3,5 +3,6 @@ package br.com.zup.Restaurante;
 public class Main {
     public static void main(String[] args) {
         Sistema.menu();
+        Sistema.cadastrarPrato();
     }
 }
--- src/br/com/zup/Restaurante/Sistema.java
+++ src/br/com/zup/Restaurante/Sistema.java
@@ -1,10 +1,28 @@
 package br.com.zup.Restaurante;
 
+import java.util.Scanner;
+
 public class Sistema {
+
+    private static Scanner capturarDados(String mensagem){
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
+
+    //opções
     public static void menu() {
         System.out.println("*** RESTAURANTE TEMPERO DE MAINHA ***");
         System.out.println("Digite 1 - Para cadastrar um prato.");
-        System.out.println("Digite 2 - Para exibir os pratos cadastrados.");
-        System.out.println("Digite 3 - Para sair do Restaurante.");
+        System.out.println("Digite 2 - Para adicionar um ingrediente.");
+        System.out.println("Digite 3 - Para exibir os pratos cadastrados.");
+        System.out.println("Digite 4 - Para sair do Restaurante.");
+
+    }
+    //cadastrar pratos
+    public static Prato cadastrarPrato(){
+        String nome = capturarDados ("Digite o nome do prato: ").nextLine();
+        double valor = capturarDados("Digite o valor do prato: ").nextDouble();
+        Prato pratos = new Prato(nome, valor);
+        return pratos;
     }
 }
