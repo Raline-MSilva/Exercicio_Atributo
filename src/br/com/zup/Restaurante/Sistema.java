f44f001d9e7d3a26b08d5a0ba313291445224cc9
Raline Silva <ralinesilva1@gmail.com>
2021-09-28 21:03:00 -0300

    Classe Sistema

---
+++ src/br/com/zup/Restaurante/Sistema.java
@@ -0,0 +1,10 @@
+package br.com.zup.Restaurante;
+
+public class Sistema {
+    public static void menu() {
+        System.out.println("*** RESTAURANTE TEMPERO DE MAINHA ***");
+        System.out.println("Digite 1 - Para cadastrar um prato.");
+        System.out.println("Digite 2 - Para exibir os pratos cadastrados.");
+        System.out.println("Digite 3 - Para sair do Restaurante.");
+    }
+}
