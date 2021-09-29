89facd8fc05e705956af4296293b84313584ec35
Raline Silva <ralinesilva1@gmail.com>
2021-09-29 10:05:06 -0300

    Entrega minima

--- src/br/com/zup/Restaurante/Sistema.java
+++ src/br/com/zup/Restaurante/Sistema.java
@@ -13,8 +13,8 @@ public class Sistema {
     public static void menu() {
         System.out.println("*** RESTAURANTE TEMPERO DE MAINHA ***");
         System.out.println("Digite 1 - Para cadastrar um prato.");
-        System.out.println("Digite 3 - Para exibir o Cardápio.");
-        System.out.println("Digite 4 - Para sair do Restaurante.");
+        System.out.println("Digite 2 - Para exibir o Cardápio.");
+        System.out.println("Digite 3 - Para sair do Restaurante.");
 
     }
     //cadastrar pratos
@@ -32,16 +32,23 @@ public class Sistema {
     //executando as opções
     public static void Executar(){
         boolean menu = true;
-        Restaurante restaurante = new Restaurante();
+        Cardápio cardapio = new Cardápio();
 
         while (menu){
             menu();
             int opcaoDesejada = capturarDados("Digite a opção que deseja: ").nextInt();
 
+            //navegando pelo menu
             if (opcaoDesejada == 1){
                 Prato pratoNovo = cadastrarPrato();
                 pratoNovo.addIngrediente(cadastrarIngrediente());
+                cardapio.addPrato(pratoNovo);
                 System.out.println(pratoNovo);
+            } else if (opcaoDesejada == 2){
+                System.out.println(cardapio);
+            }else if (opcaoDesejada == 3){
+                System.out.println("Até mais!");
+                menu = false;
             }
         }
     }
